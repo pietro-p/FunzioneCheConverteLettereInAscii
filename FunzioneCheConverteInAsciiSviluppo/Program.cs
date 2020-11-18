39c71e3dfdbf4d32773301d8029d1cd79403c66d
_ Trentini _ <pietrotrentini5@gmail.com>
2020-11-18 18:00:45 +0100

    sviluppo

--- FunzioneCheConverteInAsciiSviluppo/Program.cs
+++ FunzioneCheConverteInAsciiSviluppo/Program.cs
@@ -6,16 +6,14 @@ namespace FunzioneCheConverteInAsciiSviluppo
     {
         static void Main(string[] args)
         {
-            Console.Write("inserisci un carattere: ");
-            char input = char.Parse(Console.ReadLine());
-            int value = Convert.ToInt32(input);
-            Console.WriteLine($"decimal value of {input} is {value}");
-            string hexOutput = String.Format("{0:X}", value);
-            Console.WriteLine($"Hexadecimal value of {input} is {hexOutput}");
-            string octave = Convert.ToString(input, 8);
-            Console.WriteLine($"octave value of {input} is {octave}");
-            string binary = Convert.ToString(input, 2);
-            Console.WriteLine($"binary value of {input} is {binary}");
+            char ascii;
+            int num;
+            Console.Write("inserisci il codice di una lettera: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            ascii = Convert.ToChar(num);
+            Console.WriteLine($"{ascii}");
+            Console.ReadLine();
         }
+        
     }
 }
